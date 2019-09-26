86c521b2c1a9dec3fb5544ed5477d0e5abd4c955
Rémy HUBSCHER <remy@chefclub.tv>
2019-09-26 18:15:19 +0200

    Use a Browser.element to export ports.

--- src/ModuleA.elm
+++ src/ModuleA.elm
@@ -1,9 +1,55 @@
 port module ModuleA exposing (main)
 
-import Html exposing (text)
-import Platform.Sub as Sub
+import Browser
+import Html exposing (Html, button, text)
+import Html.Events exposing (onClick)
+
 
 port incomePort : (String -> msg) -> Sub msg
 
+
+port outcomePort : String -> Cmd msg
+
+
+type Msg
+    = Save
+    | Load String
+
+
+type alias Model =
+    { name : String
+    }
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Save ->
+            ( model, outcomePort model.name )
+
+        Load name ->
+            ( { model | name = name }, Cmd.none )
+
+
+view : Model -> Html Msg
+view model =
+    button [ onClick Save ] [ text ("ModuleA, name is: " ++ model.name) ]
+
+
+type alias Flags =
+    { name : String }
+
+
+init : Flags -> ( Model, Cmd Msg )
+init flags =
+    ( Model flags.name, Cmd.none )
+
+
+main : Program Flags Model Msg
 main =
-  text "Hello"
+    Browser.element
+        { init = init
+        , update = update
+        , subscriptions = \_ -> incomePort Load
+        , view = view
+        }
--- src/ModuleB.elm
+++ src/ModuleB.elm
@@ -1,9 +1,55 @@
 port module ModuleB exposing (main)
 
-import Html exposing (text)
-import Platform.Sub as Sub
+import Browser
+import Html exposing (Html, a, text)
+import Html.Events exposing (onClick)
+
 
 port incomePort : (String -> msg) -> Sub msg
 
+
+port outcomePort : String -> Cmd msg
+
+
+type Msg
+    = Save
+    | Load String
+
+
+type alias Model =
+    { name : String
+    }
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Save ->
+            ( model, outcomePort model.name )
+
+        Load name ->
+            ( { model | name = name }, Cmd.none )
+
+
+view : Model -> Html Msg
+view model =
+    a [ onClick Save ] [ text ("ModuleB, name is: " ++ model.name) ]
+
+
+type alias Flags =
+    { name : String }
+
+
+init : Flags -> ( Model, Cmd Msg )
+init flags =
+    ( Model flags.name, Cmd.none )
+
+
+main : Program Flags Model Msg
 main =
-  text "Hey!"
+    Browser.element
+        { init = init
+        , update = update
+        , subscriptions = \_ -> incomePort Load
+        , view = view
+        }
